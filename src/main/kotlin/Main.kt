e87d69e4760c6da0327d1506a6534a081151be80
Fedor Karpelevitch <fedor@gappify.com>
2025-05-28 13:55:11 -0700

    with distinct on every level :-/

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,7 @@
 package net.karpelevitch
 
 import kotlin.math.abs
+import kotlin.math.max
 import kotlin.time.TimeSource.Monotonic.markNow
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
@@ -8,8 +9,9 @@ import kotlin.time.TimeSource.Monotonic.markNow
 fun main(args: Array<String>) {
     if (args.isNotEmpty()) {
         if (args[0] == "first") {
+            var maxK = 3
             for (n in 1..20000) {
-                printFirstSolution(n)
+                maxK = max(maxK, printFirstSolution(n, startK = maxK - 2))
             }
         } else {
             val n = args[0].toInt()
@@ -17,16 +19,17 @@ fun main(args: Array<String>) {
             printAllSolutions(n, startK)
         }
     } else {
+        var maxK = 3
         for (n in 1..20000) {
-            printAllSolutions(n)
+            maxK = max(maxK, printAllSolutions(n, maxK - 2))
         }
     }
 }
 
-private fun printAllSolutions(n: Int, startK: Int = 1) {
+private fun printAllSolutions(n: Int, startK: Int = 1): Int {
     val start = markNow()
     val state = State(n)
-    for (k in startK..n) {
+    for (k in startK.. n) {
         var count = 0L;
         val solutions = ArrayList<State>()
         val nextStates = state.nextStates(k)
@@ -50,92 +53,85 @@ private fun printAllSolutions(n: Int, startK: Int = 1) {
                 }
             }
 
-            break
+            return k
         }
         println("Can't solve $n in $k moves t=${start.elapsedNow()}")
     }
+    throw IllegalStateException("Can't solve $n in $n moves")
 }
 
 
-private fun printFirstSolution(n: Int, state: State = State(n)) {
+private fun printFirstSolution(n: Int, state: State = State(n), startK: Int):Int {
     val start = markNow()
-    for (k in 1..n) {
+    for (k in startK..n) {
         val nextStates = state.nextStates(k)
         val solution = nextStates.firstOrNull()
         if (solution != null) {
             println("Can solve $n in $k moves with: ${solution.moves} t=${start.elapsedNow()}")
-            break
+            require(k==1 || k>startK)
+            return k
         }
 //        println("Can't solve $n in $k moves t=${start.elapsedNow()}")
     }
+    throw IllegalStateException("Can't solve $n in $n moves")
+
 }
 
-data class State(
-    val state: Map<Int, Int>,
-    val moves: List<Move>,
-    val bl: Set<Pair<Int, Int>>
-) {
-    constructor(count: Int) : this(sortedMapOf(Pair(0, count)), emptyList(), emptySet())
-
-    val stations = moves.map(Move::from).distinct().sorted().filterNot { it == 0 || it == 1 }
-
-    data class Move(val from: Int, val to: Int) {
-        override fun toString(): String {
-            return "$from->$to(${abs(from - to)})"
-        }
-
-        fun reverse() = Move(1 - this.to, 1 - this.from)
-
+data class Move(val from: Int, val to: Int) : Comparable<Move> {
+    val count = abs(from - to)
+    override fun toString(): String {
+        return "$from->$to(${abs(from - to)})"
     }
 
-    fun nextStates(movesLeft: Int): Sequence<State> {
-//        println("nextStates for $this in $movesLeft")
-        if (movesLeft < state.entries.count { it.key != 1 && it.value > 0 }) {
-            return emptySequence() // not enough moves left to move all coins to target
-        }
-        if (movesLeft == 0) {
-            return sequenceOf(this)
-        }
+    fun reverse() = Move(1 - this.to, 1 - this.from)
 
+    override fun compareTo(other: Move): Int {
+        return compareValuesBy(this, other, { it.from }, { it.to })
+    }
+}
 
-        val possibleMoves = state.keys.sortedDescending().flatMap { from ->
+data class State(val state: Map<Int, Int>, val moves: List<Move>, val count: Int) {
+    constructor(count: Int) : this(sortedMapOf(Pair(0, count)), emptyList(), count)
+
+    val stations = moves.map(Move::from).distinct().sorted().filterNot { it == 0 || it == 1 }
+    val sortedMoves = moves.sorted()
+
+    fun nextStates1(movesLeft: Int): Sequence<State> {
+        return state.keys.sortedDescending().asSequence().flatMap { from ->
             val coinsLeft = state[from]!!
             (from + coinsLeft downTo from - coinsLeft)
                 .filterNot { it == from }
-                .map { from to it }
+                .map { Move(from, it) }
+        }.mapNotNull{move(it, movesLeft)}
+    }
+
+    fun nextStates2(states: Sequence<State>, movesLeft: Int): Sequence<State> {
+        if (movesLeft==0) return states
+        return nextStates2(states.flatMap {  it.nextStates1(movesLeft)}.distinctBy { it.sortedMoves }, movesLeft - 1)
+    }
+
+    fun nextStates(movesLeft: Int): Sequence<State> {
+       return nextStates2(sequenceOf(this), movesLeft)
+    }
+
+    private fun move(m: Move, movesLeft: Int): State? {
+        val newState = state.toMutableMap()
+        val coins = m.count
+        val destCount = newState.getOrDefault(m.to, 0)
+
+        newState[m.to] = destCount + coins
+        val newFromCount = state[m.from]!! - coins
+        require(newFromCount >= 0)
+        if (newFromCount == 0) {
+            newState.remove(m.from)
+        } else {
+            newState[m.from] = newFromCount
         }
-
-        val bbl = bl.intersect(possibleMoves)
-
-        val moves = possibleMoves.filterNot { bbl.contains(it) }
-
-        return (0 until moves.size).asSequence()
-            .flatMap {
-                val from = moves[it].first
-                val to = moves[it].second
-                require(from != to)
-                //        println("move: $from -> $to, movesLeft: $movesLeft")
-                val newState = state.toMutableMap()
-                val coins = abs(from - to)
-                val destCount = newState.getOrDefault(to, 0)
-                //        if (destCount > 0 && moves.last { it.to == to }.from > from) {
-//            return emptySequence()
-//        }
-                newState[to] = destCount + coins
-                val newFromCount = state[from]!! - coins
-                require(newFromCount >= 0)
-                if (newFromCount == 0) {
-                    newState.remove(from)
-                } else {
-                    newState[from] = newFromCount
-                }
-                if (newState.count { it.key != 1 && it.value > 0 } > movesLeft - 1) {
-                    emptySequence()
-                } else {
-                    sequenceOf(State(newState.toMap(), this.moves + Move(from, to), bbl + moves.subList(0, it)))
-                }
-            }
-            .flatMap { it.nextStates(movesLeft - 1) }
+        return if (newState.count { it.key != 1 && it.value > 0 } > movesLeft - 1) {
+            null
+        } else {
+            State(newState.toMap(), this.moves + m, this.count)
+        }
     }
 
 }
